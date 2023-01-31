ad83e90f4a74bbcfb78f245657e6fc43cecd622c
Piryanshu88 <deepubisht2004@gmail.com>
2023-01-31 16:54:50 +0530

    adding solutionn for sub array

---
+++ sub_array.js
@@ -0,0 +1,65 @@
+// boiler plate for js 
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+// actual code 
+// we have to make subarr length of m and check whether the sum is equal to d or not 
+function birthday(s, d, m) {
+    // Write your code here
+    let count=0
+    
+    for(let i=0;i<s.length;i++){
+      let sum=0
+      for(let j=0;j<m;j++){
+        sum=sum+s[i+j]
+      }
+      if(sum==d){
+        count++
+      }
+    }
+     return count
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const s = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const d = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    const result = birthday(s, d, m);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
