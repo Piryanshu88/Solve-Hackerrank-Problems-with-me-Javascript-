6ad0f1a89a4d5b47641258ceb415c89b2b609d82
Piryanshu88 <deepubisht2004@gmail.com>
2023-01-31 16:57:43 +0530

    adding division sum pairs

---
+++ division_sum_pair.js
@@ -0,0 +1,67 @@
+// boiler plate for js 
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'divisibleSumPairs' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY ar
+ */
+
+// actual code
+function divisibleSumPairs(n, k, ar) {
+    // Write your code here
+let count=0
+for(let i=0;i<n;i++){
+  for(let j=i+1;j<n;j++){
+    // checking two diff element sum is divisible by k and if yes then increment the count 
+    if((ar[i]+ar[j])%k==0){
+      count++
+    }
+  }
+}
+return count
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const k = parseInt(firstMultipleInput[1], 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = divisibleSumPairs(n, k, ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
