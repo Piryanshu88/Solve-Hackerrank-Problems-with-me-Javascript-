4b4791a24d3a7760048db386eda52b258926b329
Piryanshu88 <deepubisht2004@gmail.com>
2023-01-31 15:38:24 +0530

    adidng solution for two str

---
+++ two_str.js
@@ -0,0 +1,64 @@
+// boiler plate for js 
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'twoStrings' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s1
+ *  2. STRING s2
+ */
+
+// actual code for problem 
+// In this problem we have to checking wheter s1 and s2 have common substring
+function twoStrings(s1, s2) {
+    // Write your code here
+  for(let i=0;i<s1.length;i++){
+    //checking whether s1 letter occur in s2
+    if(s2.includes(s1[i])){
+      return "YES"
+    }
+  }
+  return "NO"
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s1 = readLine();
+
+        const s2 = readLine();
+
+        const result = twoStrings(s1, s2);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
