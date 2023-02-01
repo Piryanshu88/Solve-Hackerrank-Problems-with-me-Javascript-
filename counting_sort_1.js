b1e18d370948c67af80bbcaf1d958c61a00bb716
Piryanshu88 <deepubisht2004@gmail.com>
2023-02-01 22:46:06 +0530

    adding solution for counting sort 1

---
+++ counting_sort_1.js
@@ -0,0 +1,62 @@
+// boiler plate for js 
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+/*
+we have to do an operation like this 
+let arr=[1,1,3,2,1]
+
+i	arr[i]	result
+0	1	[0, 1, 0, 0]
+1	1	[0, 2, 0, 0]
+2	3	[0, 2, 0, 1]
+3	2	[0, 2, 1, 1]
+4	1	[0, 3, 1, 1]
+ */
+function countingSort(arr) {
+    // Write your code here
+    
+    let newarr=new Array(arr.length).fill(0)
+    for(let i=0;i<arr.length;i++){
+        //for everytime an index appear it increment it value 
+      newarr[arr[i]]++
+    }
+    return newarr.splice(0,100)
+  
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = countingSort(arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
