c835ad725b617d0999fd2be7e6fb4b1f65cc849e
Piryanshu88 <deepubisht2004@gmail.com>
2023-02-04 16:14:54 +0530

    solution for electronic shop

---
+++ electronic_shop.js
@@ -0,0 +1,77 @@
+// boiler plaate for js 
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the getMoneySpent function below.
+ */
+function getMoneySpent(keyboards, drives, b) {
+    /*
+     * Write your code here.
+     * k
+     * 
+     */
+    
+    // b is budget
+    // find the diff the most expensive computer keyboard and USB drive that can be purchased with a give budget.
+    keyboards.sort((a,b)=>a-b)
+    drives.sort((a,b)=>a-b)
+    let max=-1
+    for(let i=0;i<keyboards.length;i++){
+      for(let j=0;j<drives.length;j++){
+        let sum=keyboards[i]+drives[j]
+        if(max<sum&&sum<=b){
+          max=sum
+        }
+      }
+    }
+    return max
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const bnm = readLine().split(' ');
+
+    const b = parseInt(bnm[0], 10);
+
+    const n = parseInt(bnm[1], 10);
+
+    const m = parseInt(bnm[2], 10);
+
+    const keyboards = readLine().split(' ').map(keyboardsTemp => parseInt(keyboardsTemp, 10));
+
+    const drives = readLine().split(' ').map(drivesTemp => parseInt(drivesTemp, 10));
+
+    /*
+     * The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+     */
+
+    let moneySpent = getMoneySpent(keyboards, drives, b);
+
+    ws.write(moneySpent + "\n");
+
+    ws.end();
+}
