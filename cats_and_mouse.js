8bb0fabf6782295add917386e258c8b6acadbe8d
Piryanshu88 <deepubisht2004@gmail.com>
2023-02-04 16:19:29 +0530

    adding solution for cats and mouse

---
+++ cats_and_mouse.js
@@ -0,0 +1,60 @@
+// boiler plate for js 
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the catAndMouse function below.
+function catAndMouse(x, y, z) {
+if(Math.abs(x-z)==Math.abs(y-z)){
+  return "Mouse C"
+}else if(Math.abs(x-z)>Math.abs(y-z)){
+  return "Cat B"
+}else if(Math.abs(x-z)<Math.abs(y-z)){
+  return "Cat A"
+}
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const xyz = readLine().split(' ');
+
+        const x = parseInt(xyz[0], 10);
+
+        const y = parseInt(xyz[1], 10);
+
+        const z = parseInt(xyz[2], 10);
+
+        let result = catAndMouse(x, y, z);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
