d6858413cc08f3f508922286d7e93b66f7e54fa1
Piryanshu88 <deepubisht2004@gmail.com>
2023-02-07 20:04:30 +0530

    adding solution for beautiful day at movies

---
+++ beautiful_day_at_movies.js
@@ -0,0 +1,71 @@
+// boiler plate for js 
+// boiler plate for js 
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'beautifulDays' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER i
+ *  2. INTEGER j
+ *  3. INTEGER k
+ */
+
+function beautifulDays(i, j, k) {
+    // Write your code here
+    // we have to find the no of beautiful number, that is if the diff of number and it reverse is divisible by k the it is said beautiful number 
+    let count=0
+    while(i<=j){
+      // reversing the number 
+     let revnumber=+i.toString().split("").reverse().join("")
+     // checking if the diff is divisible by k or not 
+     if(Math.abs(revnumber-i)%k==0){
+       count++
+     }
+     i++
+      
+    }
+    return count
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const i = parseInt(firstMultipleInput[0], 10);
+
+    const j = parseInt(firstMultipleInput[1], 10);
+
+    const k = parseInt(firstMultipleInput[2], 10);
+
+    const result = beautifulDays(i, j, k);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
