86b60d4845150e3fdc2899a32e322c9bff630c3d
Piryanshu88 <deepubisht2004@gmail.com>
2023-02-07 19:43:40 +0530

    adding solution for climbng the stairs

---
+++ climbing_the_leaderboard.js
@@ -0,0 +1,73 @@
+// boiler plate for js
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'climbingLeaderboard' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY ranked
+ *  2. INTEGER_ARRAY player
+ */
+
+function climbingLeaderboard(ranked, player) {
+    // Write your code here
+    let newranking=[...new Set(ranked)]
+    // remove duplicate from rankings
+    
+    
+    let rank=newranking.length+1
+    // because it is zero based indexing so adding 1
+  
+    let result=[]
+    for(let p in  player){
+      while(player[p]>=newranking[rank]){
+        // decrement rank till it is greater than newranking
+        rank--
+      }
+      // and at last adding it to result
+      result.push(rank)
+    }
+  return result
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const rankedCount = parseInt(readLine().trim(), 10);
+
+    const ranked = readLine().replace(/\s+$/g, '').split(' ').map(rankedTemp => parseInt(rankedTemp, 10));
+
+    const playerCount = parseInt(readLine().trim(), 10);
+
+    const player = readLine().replace(/\s+$/g, '').split(' ').map(playerTemp => parseInt(playerTemp, 10));
+
+    const result = climbingLeaderboard(ranked, player);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
+
