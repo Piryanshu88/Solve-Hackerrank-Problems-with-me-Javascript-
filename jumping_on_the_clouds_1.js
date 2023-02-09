9e90cdcfa8071fffd847147450bcf9fcf77d42f8
Piryanshu88 <deepubisht2004@gmail.com>
2023-02-09 22:43:30 +0530

    adding solution for jumping on the clouds part1

---
+++ jumping_on_the_clouds_1.js
@@ -0,0 +1,63 @@
+// boiler plate for js 
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'jumpingOnClouds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY c as parameter.
+ */
+
+function jumpingOnClouds(c) {
+    // Write your code here
+let i=0
+let count=0
+// we can jump on 1 but not in 0's
+// we have to calculate minimum number of jumps it will take to jump from the starting postion to the last cloud. 
+while(i<c.length){
+  if(c[i+2]==0){
+    count++
+    i=i+2
+  }else{
+    i=i+1
+    count++
+  }
+}
+return count-1
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const c = readLine().replace(/\s+$/g, '').split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    const result = jumpingOnClouds(c);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
