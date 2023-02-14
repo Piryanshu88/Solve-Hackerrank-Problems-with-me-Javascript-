7f87533e11c500e02532c759de963477858f06ed
Piryanshu88 <deepubisht2004@gmail.com>
2023-02-14 18:58:26 +0530

    adding solution for minimum absolute diff

---
+++ minimum_absolute_difference_in_arr.js
@@ -0,0 +1,60 @@
+// boiler plate for js 
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'minimumAbsoluteDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function minimumAbsoluteDifference(arr) {
+    // Write your code here
+    // we have to find the minimum abs diff with two elements 
+    let  min=Infinity
+    for(let i=0;i<arr.length;i++){
+      for(let j=i+1;j<arr.length;j++){
+        if(Math.abs(arr[i]-arr[j])<min){
+          min=Math.abs(arr[i]-arr[j])
+        }
+      }
+    }
+    return min
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = minimumAbsoluteDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
